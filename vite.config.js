0049e916907e306ac81d9a693387051ff9370243
snsar <lethoquyen2002@gmail.com>
2024-12-22 15:34:03 +0700

    Enhance Vite configuration with advanced proxy error handling and logging
    - Added rewrite rule for API paths in Vite server proxy configuration.
    - Implemented error handling and logging for proxy requests and responses to improve debugging and monitoring of API interactions.

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,22 @@ export default defineConfig({
         target: "http://34.92.164.246:9090",
         changeOrigin: true,
         secure: false,
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
+        configure: (proxy, _options) => {
+          proxy.on("error", (err, _req, _res) => {
+            console.log("proxy error", err);
+          });
+          proxy.on("proxyReq", (proxyReq, req, _res) => {
+            console.log("Sending Request to the Target:", req.method, req.url);
+          });
+          proxy.on("proxyRes", (proxyRes, req, _res) => {
+            console.log(
+              "Received Response from the Target:",
+              proxyRes.statusCode,
+              req.url
+            );
+          });
+        },
       },
       "/query": {
         target:
